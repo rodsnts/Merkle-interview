fad25481a5961a955cfed0fe8e48eef72ceaefb5
Rodrigo Santos <rr.santos1232@gmail.com>
2020-07-16 14:24:47 +0100

    Update Recursive Reverse String.js

--- Recursive Reverse String.js
+++ Recursive Reverse String.js
@@ -1,3 +1,4 @@
+// My thoughts on my solution: This function has a time complexity of O(n^3) and that's not the ideal way to do it but was the only way i managed to do
 const reverseString = (str) => {
   if (str.length === 1 || str.length === 0) {
     return str;
